88e6b2465994b71c2e7cb4830d695c510d4bbc4e
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-01-30 17:20:01 +0600

    #uiStart

---
+++ Cinemate/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  Cinemate
+//
+//  Created by Md. Mehedi Hasan on 30/1/20.
+//  Copyright © 2020 Md. Mehedi Hasan. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var confirmPasswordInput: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    
+
+}
