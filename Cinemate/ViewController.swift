3fb39d77a2b0fd86c2c6d6a36a374a23202e075d
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-01-30 15:42:51 +0600

    Initial Commit

---
+++ Cinemate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinemate
+//
+//  Created by Md. Mehedi Hasan on 30/1/20.
+//  Copyright © 2020 Md. Mehedi Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CinemateTests/CinemateTests.swift
@@ -0,0 +1,34 @@
+//
+//  CinemateTests.swift
+//  CinemateTests
+//
+//  Created by Md. Mehedi Hasan on 30/1/20.
+//  Copyright © 2020 Md. Mehedi Hasan. All rights reserved.
+//
+
+import XCTest
+@testable import Cinemate
+
+class CinemateTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
