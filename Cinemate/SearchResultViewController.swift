72a8a1b4e9594c261f916be31417fba1da5f06fe
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-02-02 15:30:43 +0600

    #minor

--- Cinemate/SearchResultViewController.swift
+++ Cinemate/SearchResultViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class SearchResultViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var imdbRatingLabel: UILabel!
+    @IBOutlet weak var tomatoesRatingLabel: UILabel!
+    @IBOutlet weak var MetacriticRatingLabel: UILabel!
+    @IBOutlet weak var plotTextView: UITextView!
+    @IBOutlet weak var actorsTextView: UITextView!
+    @IBOutlet var productionLabel: UIView!
+    @IBOutlet weak var boxOfficeLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +29,6 @@ class SearchResultViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
