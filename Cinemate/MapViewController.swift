f9f418ccc11c898b018336fc3a484c533b9d0fdc
Mehedi Hasan <m.mehedi_hasan@hotmail.com>
2020-02-09 14:36:55 +0600

    #mapUi

---
+++ Cinemate/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  Cinemate
+//
+//  Created by Md. Mehedi Hasan on 9/2/20.
+//  Copyright © 2020 Md. Mehedi Hasan. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
